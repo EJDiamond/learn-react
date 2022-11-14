e45f92515c25d68a5fd07fa4544f36d524070f0b
EJDiamond <emilydiamond321@gmail.com>
2022-11-14 20:15:18 +0000

    Add event binder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,15 +5,21 @@ import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProp
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
+import EventBinding from './components/EventBinding';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
+      {/* <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting='nice to meet you!' name="Mike" age="32" />
-      <StatefulGreeting greeting="I'm a stateful class component" name="Mike"/>
-      <StatefulGreetingWithCallback greeting="I'm a stateful class component" name="Mike"/>
-      <StatefulGreetingWithPrevState greeting="I'm a stateful class component" name="Mike"/>
+      <StatefulGreeting greeting="I'm a stateful class component" name="Mike" />
+      <StatefulGreetingWithCallback greeting="I'm a stateful class component" name="Mike" />
+      <StatefulGreetingWithPrevState greeting="I'm a stateful class component" name="Mike" />
+      <EventsFunctional />
+      <EventsClass /> */}
+      <EventBinding />
     </div>
   );
 }
---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,56 @@
+// rfce tab - creates and exports component
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rafce - creates and exports component as an arrow function
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rce - create and export a class component
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippet
+
+// rconst - creates constructor with setState
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+// clg - logs to the console
+// console.log(first)
+
+
+// clo - logs object with its name as a string
+// console.log('first', first)
